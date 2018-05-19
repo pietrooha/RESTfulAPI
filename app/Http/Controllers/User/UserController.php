09d5cc5c493bcbb72bd71783cd8d18b572e1dd03
Piotr Czerwiński <pwkczerwinski@gmail.com>
2018-05-19 22:43:17 +0200

    Added re-sending the verification email if requested functionality

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Mail\UserCreated;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends ApiController
 {
@@ -126,5 +128,16 @@ class UserController extends ApiController
         $user->save();
 
         return $this->showMessage('The account has been verified successfully');
+    }
+
+    public function resend(User $user)
+    {
+        if ($user->isVerified()) {
+            return $this->errorResponse('This user is already verified', 409);
+        }
+
+        Mail::to($user)->send(new UserCreated($user));
+
+        return $this->showMessage('The verification email has been resend');
     }
 }
