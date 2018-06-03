a04f0523aca1c254cb1a8cba7a1ac243c1630b0d
Piotr Czerwiński <pwkczerwinski@gmail.com>
2018-06-03 23:47:09 +0200

    Implemented the restrictions over user

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,7 +5,9 @@ namespace App\Providers;
 use App\Buyer;
 use App\Policies\BuyerPolicy;
 use App\Policies\SellerPolicy;
+use App\Policies\UserPolicy;
 use App\Seller;
+use App\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -20,7 +22,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Buyer::class => BuyerPolicy::class,
-        Seller::class => SellerPolicy::class
+        Seller::class => SellerPolicy::class,
+        User::class => UserPolicy::class
     ];
 
     /**
